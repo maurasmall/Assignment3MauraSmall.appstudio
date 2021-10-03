ef50f21d9bb133fb549e7f0c536d764ceba99613
Maura Small <maurasmall@creighton.edu>
2021-10-03 13:08:03 -0500

    while loops to enter city names

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,60 @@
+let cityName = ""
+let cities = [""]
+let doContinue = confirm(`Do you want to add another city? OK for Yes or Cancel for No`)
+while (doContinue) {
+    cityName = prompt("Enter new city name:")
+    cities.push(cityName)
+    doContinue = confirm(`Do you want to add another city? OK for Yes or Cancel for No`)
+  }
+console.log(`${cities}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let cities = ["Omaha"]
+let cityName = ""
+btnSubmit.onclick=function() {
+  let cityName = inptCity.value
+  for (i = 0; i < cities.length; i++)  {
+    while (btnSubmit.onclick) {
+      cities.push(cityName)
+      }
+      console.log(`${cities}`)
+    }
+    }
+  
+if (cities[i] == cityName) {
+   lblMessage.value =  "City already added."  
+}
+else { 
+  cities.push()
+  lblMessage.value = cityName + " has been added."
+  }
+  } 
+  }
+*/
+
+
